28f5ad74ecb1bd7430a351b486f219a7ffe696de
gautamrwx <gautam.404@protonmail.com>
2020-05-20 01:22:41 +0530

    Persistance using mongodb

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 const logger = require('./middleware/logger')
 const genres = require('./routes/genres')
 const home = require('./routes/home')
 
+mongoose.connect('mongodb://localhost/vidly',{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true })
+    .then(()=> console.log('Connected MongoDB'))
+    .catch(() => console.error('Could Not Connect'));
 
 app.use(express.json())
 
--- routes/genres.js
+++ routes/genres.js
@@ -1,69 +1,66 @@
 const joi = require('joi');
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-const genres = [
-    {id:1,name:'Action'},
-    {id:2,name:'SciFi'},
-    {id:3,name:'Comedy'}
-];
+const genreSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minlength:3,
+        maxlength:50
+    }
+});
+
+const Genre = new mongoose.model('Genre',genreSchema);
 
 // Read Resource
-router.get('/',(req,res) => {
+router.get('/',async (req,res) => {
+    const genres = await Genre.find();
     res.send(genres);
 })
 
-router.get('/:id',(req,res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
+router.get('/:id',async (req,res) => {
+    const genre = await Genre.findById(req.params.id);
     if(!genre) return res.send("Not Found");
 
     res.send(genre);
 })
 
 // Create Resource
-router.post('/',(req,res) => {
-
-    // const result = validateGenre(req.body) Works same as result.error
+router.post('/',async (req,res) => {
     const {error} = validateGenre(req.body);
     if(error) return res.send(error.details[0].message);
 
-    const genre = {
-        id:genres[genres.length-1].id+1,
-        name:req.body.name
-    };
-
-    genres.push(genre);
+    let genre = new Genre({name:req.body.name});
+    genre = await genre.save();
     res.send(genre);
 })
 
 // Update Resource
-router.put('/:id',(req,res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
+router.put('/:id',async (req,res) => {
+    const {error} = validateGenre(req.body);
+    if(error) return res.send(error.details[0].message);
+
+    const genre = await Genre.findByIdAndUpdate(req.params.id,{name:req.body.name},{new:true});
     if(!genre) return res.send("Not Found");
 
-    // const result = validateGenre(req.body) Works same as result.error
-    const {error} = validateGenre(req.body)
-    if(error) return res.send(error.details[0].message);
-    
     genre.name = req.body.name;
-    res.send(genre)
+    res.send(genre);
 })
 
 // Delete Resource
-router.delete('/:id',(req,res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
+router.delete('/:id',async (req,res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id);
     if(!genre) return res.send("Not Found");
 
-    const index = genres.indexOf(genre)
-    genres.splice(index,1)
-
-    res.send(genre)
+    res.send(genre);
 })
 
 // -- Req Body Validation Logic -- //
 function validateGenre(genre){
     const schema = {
-        name:joi.string().min(3).required()
+        name:joi.string().min(3).max(50).required()
     };
 
     return joi.validate(genre,schema);
